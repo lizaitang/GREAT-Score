c1a71fad58ca038b3c337bec03e1e8b05f7dd4ef
lizaitanng <970015316lzt@gmail.com>
2022-05-25 22:27:47 +0800

    ’log1message‘

--- formulated.py
+++ formulated.py
@@ -18,9 +18,19 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from robustbench.data import load_cifar10
+from robustbench.utils import load_model, clean_accuracy
+import datetime
+import numpy as np
+import warnings
+
+warnings.filterwarnings(action='ignore')
+
+import torch.nn as nn
+import torch.optim as optim
 
 device = torch.device("cuda")
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 mpl.use('Agg')
 from scipy import stats
 import numpy as np
@@ -36,7 +46,7 @@ from utils import l2_distance,softmax,tempsigmoid
 
 
 def main():
-    parser = argparse.ArgumentParser(description='cig-nn')
+    parser = argparse.ArgumentParser(description='GREAT')
     # dataset: cifar-10 default and imagenet ,set -data_dir names according to the dataset
     # different group setting : 1,2,3,4,5 group and comment, follows and output.
     # set a sample directory contains the sample npz or records the command for creating the samples?
@@ -48,54 +58,150 @@ def main():
     # set different size to process the image. eg:255 -0.5
     # untargeted cw attack and targeted version
     # 
-    parser = ArgumentParser(add_help=True)
     parser.add_argument('--dataset',type=str,default='cifar10',description='the dataset want to evaluate on: ›cifar10 or imagenet')
+    parser.add_argument('--activation_function',type=str,default='sigmoid',description='the activation function we used')
     parser.add_argument('--sample_size',type=int,default='1000',description= 'the generated model sample size')
-    parser.add_argument('--num_layers', type=int, default=3)
-    parser.add_argument('--hidden_channels', type=int, default=128)
-    # put a layer norm right after input
-    parser.add_argument('--layer_norm_first', action="store_true")
-    # put layer norm between layers or not
-    parser.add_argument('--use_ln', type=int,default=0)
-    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--data_path',type=str,default='samples.npz',description='the samples data path')
+    
     parser.add_argument('--lr', type=float, default=0.01)
-    parser.add_argument('--l2decay', type=float, default=0.0)
-    parser.add_argument('--epochs', type=int, default=500)
-    parser.add_argument('--runs', type=int, default=10)
-    parser.add_argument('--seed', type=int, default=0)
-
-    #print device information if set true
-    parser.add_argument('--reprod', action="store_true")
-    parser.add_argument('--inductive', action="store_true")
+    
     
 
     # attack specific settings
     parser.add_argument('--lower_bound_eval_global', action="store_false",description='whether to run cw attack on overall images')
     parser.add_argument('--lb_n',type=int,default=1000,description='the sample size used to test the lower bound')
     parser.add_argument('--robust_accuracy', action="store_false", description='whether to run robust accuracy evaluation on over images')
-    parser.add_argument('--ra_n',type=int,default=500,description='the sample size used to ')
+    parser.add_argument('--ra_n',type=int,default=500,description='the sample size used to run robust accuracy evaluation')
 
-    # save and load best weights for final evaluation
-    parser.add_argument('--best_weights', action="store_true")
-
-    ######################## Adv. Training Setting ####################
-    parser.add_argument('--step_size', type=float, default=1e-3)
-    parser.add_argument('--m', type=int, default=3)
-    parser.add_argument('--attack', type=str, default='vanilla')
-    parser.add_argument('--pre_epochs', type=int, default=-1)
-
-    ######################## Robustness Eval Setting ####################
-    parser.add_argument('--eval_robo', action="store_true")
-    # targeted attack else non-targeted
-    parser.add_argument('--eval_target', action="store_true")
-    # number of targets in each deg category
-    parser.add_argument('--target_num', type=int, default=200) 
+    args = parser.parse_args()
     
-    # if evaluated in blackbox, the attacked graph will be loaded for evaluation
-    parser.add_argument('--eval_robo_blk', action="store_true")
+    # load the generated images and process
+    path="samples.npz"
+    f=np.load(path)
+    train_X, train_y = f['x'], f['y']
+    f.close()
+    train_X = train_X.astype('float32')
+    train_X /= 255
+    #train_X=train_X-0.5
+    train_y = train_y.astype('int64')
+    train_X1=train_X[0:2000]
+    train_y=train_y[0:2000]
+    target_classes=np.zeros(2000)
+    images=torch.from_numpy(train_X1)
+    labels=torch.from_numpy(train_y)
+    X_adv_data=train_X1
+    Y_data=train_y
 
+    # set a array to store the great score for each model
+    great_result1=[]
 
+    # load different models according to the dataset
+    if args.dataset.lower() == 'cifar10':
+      model_list=['Rebuffi2021Fixing_70_16_cutmix_extra','Gowal2020Uncovering_extra','Rebuffi2021Fixing_70_16_cutmix_ddpm','Rebuffi2021Fixing_28_10_cutmix_ddpm','Augustin2020Adversarial_34_10_extra','Sehwag2021Proxy','Augustin2020Adversarial_34_10','Rade2021Helper_R18_ddpm','Rebuffi2021Fixing_R18_cutmix_ddpm','Gowal2020Uncovering', 'Sehwag2021Proxy_R18', 'Wu2020Adversarial','Augustin2020Adversarial','Engstrom2019Robustness','Rice2020Overfitting','Rony2019Decoupling','Ding2020MMA']
+    else:
+      model_list=['Salman2020Do_50_2','Salman2020Do_R50','Engstrom2019Robustness','Wong2020Fast','Salman2020Do_R18']
+    
+    for model_name in model_list:
+            start1 = datetime.datetime.now()
+            print('Model: {}'.format(model_name),file=data)
+            if args.dataset.lower() == 'cifar10':
+               model = load_model(model_name=model_name, dataset='cifar10', threat_model='L2').to(device)
+            else:
+               model = load_model(model_name=model_name, dataset='imagenet',threat_model='Linf').to(device)
+            
+            difference=np.zeros(2000,dtype=float)
 
+            seq = []
+            dif=[]
+            j=0
+            num_correct= 0
+            right=target_classes
+            for idx in range(len(Y_data)):
+                        # load adversarial image
+                        image_adv = np.array(np.expand_dims(X_adv_data[idx], axis=0), dtype=np.float32)
+                        #image_adv = np.transpose(image_adv, (0, 3, 1, 2))
+                        # load label
+                        label = np.array(Y_data[idx], dtype=np.int64)
+                        # transform to torch.tensor
+                        data_adv = torch.from_numpy(image_adv).to(device)
+                        target = torch.from_numpy(label).to(device)
+                        
+                        
+                        # evluation
+                        X, y = Variable(data_adv, requires_grad=True), Variable(target)
+                        out = model(X)
+                        out1=out.detach().cpu().numpy()
+                        #print(out,file=data)
+                        
+                        #out = tempsigmoid(out)
+                        # use different activation function according to 
+                        if args.dataset.lower() == 'cifar10':
+                           out=torch.sigmoid(out)
+                        else:
+                           out = softmax(out)
+                        #print(out,file=data)
+                        num_classes = len(out1)
+                        predicted_label =np.argmax(out1)
+                        least_likely_label = np.argmin(out1)
+                        start_class = 0 
+                        random_class = predicted_label
+                        top2_label = np.argsort(out1[0])[-2]
+                        #print(top2_label,file=data)
+                        #print(out,file=data)
+                        new_seq = [least_likely_label, top2_label, predicted_label]
+                        #print(new_seq)
+                        random_class = random.randint(start_class, start_class + num_classes - 1)
+                        new_seq[2] = random_class
+                        #true_label = np.argmax(Y_data[idx])
+                        true_label =target
+
+                        information = []
+                        target_type = 0b0001
+                        predicted_label2=np.array(predicted_label)
+                        predicted_label2=torch.from_numpy(predicted_label2).to(device)
+                    
+                        #out = softmax(out)
+                        #out=(1+torch.tanh(out))/2
+                        if true_label != predicted_label2:
+                        #print(1)
+                        #punk=1
+                        #seq.append(new_seq[1])
+                            difference[j]=0
+                            j=j+1
+                        else:
+                            if target_type & 0b10000:
+                                            for c in range(num_classes):
+                                                if c != predicted_label:
+                                                    seq.append(c)
+                                                    information.append('class'+str(c))
+                            else:
+                                            if target_type & 0b0100:
+                                                # least
+                                                seq.append(new_seq[0])
+                                                information.append('least')
+                                                difference[idx]=out[0][predicted_label]-out[0][least_likely_label]
+                                                
+                                            if target_type & 0b0001:
+                                                # top-2
+                                                seq.append(new_seq[1])
+                                                #difference[idx]=out[0][predicted_label]-out[0][top2_label]
+                                                difference[j]=math.sqrt(math.pi/2)*(out[0][predicted_label]-out[0][top2_label])
+                                                j=j+1
+                                                #dif.append(out[0][predicted_label]-out[0][top2_label])
+                                                information.append('top2')
+                                                #print(out,file=data)
+                                            if target_type & 0b0010:
+                                                # random
+                                                seq.append(new_seq[2])
+                                                difference[idx]=out[0][predicted_label]-out[0][random_class]
+                                                information.append('random')
+                        
+                            #target_classes[idx]=new_seq[1]
+                        
+                            #out = softmax(out)
+                            predicted_label1=np.array(predicted_label)
+                            predicted_label1=torch.from_numpy(predicted_label1).to(device)
+                            num_correct += torch.eq(predicted_label1, target).sum().float().item()
 
 
 
